24cb8a882bccb36c6e39a162061330e2828271a5
Gerardo Rodarte <gerardo.rodarte.013@gmail.com>
2018-06-26 18:09:21 -0700

    se creo la conexion a la mongo

---
+++ server/database.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const mongoose = require('mongoose');
+
+const URI = 'mongodb://localhost/mean-crud';
+
+mongoose.connect(URI)
+    .then(db => console.log('DB is connected'))
+    .catch(err => console.log(err));
+
+module.exports = mongoose;
---
+++ server/index.js
@@ -0,0 +1,29 @@
+'use strict'
+
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+
+const { mongoose } = require('./database');
+
+/***********************
+** Settings
+***********************/
+app.set('port', process.env.PORT || 3000);
+
+/***********************
+** Middlewares
+***********************/
+app.use(morgan('dev'));
+app.use(express.json());
+
+/***********************
+** Routes
+***********************/
+
+/***********************
+** Starting the server
+***********************/
+app.listen(app.get('port'), () => {
+    console.log('Server on port', app.get('port'));
+});
